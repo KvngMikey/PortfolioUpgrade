a662a5ed6d8bb6eab9da49bd40d4f172b9f223ba
Michael Ariwaodo <michael.ariwaodo@seech.com>
2023-02-01 22:17:48 +0100

    skills work being done

--- src/App.js
+++ src/App.js
@@ -14,15 +14,17 @@ function App() {
       <main className="max-w-[97%] lg:max-w-[85%] mx-auto">
         <About />
         <hr className="mx-auto bg-black dark:bg-white max-w-[97%]" />
-        <section id="skills" className="p-6 my-12 scroll-mt-40 widescreen:section-min-height tallscreen:section-min-height">
+        <section id="skills" className="p-6 my-12 scroll-mt-40">
         <h2 className="text-4xl font-bold text-center sm:text-5xl sm:text-left mb-6 mx-6 text-slate-900 dark:text-white">
             Skills
         </h2>
-        <ul className='className="list-none mx-auto my-12 flex flex-col sm:flex-row sm:flex-wrap items-center gap-8"'>
-          {skillsData.map(skill => {
-            return <Skills key={skill.id} image={skill.img}/>
-          })}
-        </ul>
+        <div className='grid overflow-hidden items-center w-full'>
+          <ul className='list-none mx-auto flex items-center min-w-full marquee-group'>
+            {skillsData.map(skill => {
+              return <Skills key={skill.id} image={skill.img} />
+            })}
+          </ul>
+        </div>
         </section>
         <hr className="mx-auto bg-black dark:bg-white max-w-[97%]" />
         <section id="work" className="p-6 mt-12 scroll-mt-40 widescreen:section-min-height tallscreen:section-min-height">
--- src/components/Works.js
+++ src/components/Works.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { motion } from 'framer-motion';
 import {AiFillEye} from 'react-icons/ai';
 import { FiLink } from "react-icons/fi";
 
 const Works = (props) => {
   return (
-    <li className="w-full sm:w-[46%] pl-4 flex flex-col border border-solid border-black dark:border-white bg-stone-300 dark:bg-black py-4 px-2 shadow-xl mx-4 mb-4">
+    <motion.li className="w-full sm:w-[46%] pl-4 flex flex-col border border-solid border-black dark:border-white bg-stone-300 dark:bg-black py-4 px-2 shadow-xl mx-4 mb-4"
+    whileHover={{ scale: [null, 0.6, 1.1] }}
+      transition={{ duration: 1 }}>
     <span className='tracking-wide mb-1.5 text-lg sm:text-xl capitalize font-medium'>{props.name}</span>
     <span className='tracking-tight text-xs uppercase font-medium'>{props.stack}</span>
     <span className='my-8 tracking-tight text-base font-medium'>{props.description}</span>
@@ -18,7 +21,7 @@ const Works = (props) => {
         <a href={props.code} target="_blank">Code</a>
       </div>
     </div>
-    </li>
+    </motion.li>
   )
 }
 
